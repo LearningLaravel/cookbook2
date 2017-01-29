9a83795f03e9c0eb884cea9394e057b71836deb2
Learning Laravel <support@learninglaravel.net>
2017-01-30 03:08:04 +0700

    Recipe 3

---
+++ resources/views/master.blade.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title> @yield('title') </title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+
+    <script src="//code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+@include('shared.navbar')
+
+@yield('content')
+
+</body>
+</html>
