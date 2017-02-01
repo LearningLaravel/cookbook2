1d7486fe6df8bf0be952a901c185a0a98c2d02e7
Learning Laravel <support@learninglaravel.net>
2017-02-02 00:31:08 +0700

    Recipe 10

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix' => 'v1'], function(){
+Route::group(['prefix' => 'v1', 'middleware' => 'cors'], function(){
     Route::resource('posts', 'PostsController');
 });
+
 
